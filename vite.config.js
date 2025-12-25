20005c5349c6f8b9f97079ae73bd1586c3ad6f21
Fenet-22 <fenetahmed30@gmail.com>
2025-12-25 15:40:51 +0300

    final commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Amazon-Clone-Frontend/', 
+  base: '/Amazon-Frontend/', 
   plugins: [
     react({
       babel: {
