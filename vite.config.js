bce8b61110cfc44aa60392ad3ab4764070fec299
Fenet-22 <fenetahmed30@gmail.com>
2026-01-19 02:03:54 +0300

    Final cleanup of repository structure

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // Use '/' so it works perfectly on Netlify and Localhost
+  //  '/' so it works perfectly on Netlify and Localhost
   base: '/', 
   plugins: [
     react({
